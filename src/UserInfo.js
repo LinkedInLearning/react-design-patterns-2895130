c176a05aba11b36cf7d7099705709b727b98c63f
Shaun Wassell <shaun@trove.ai>
2021-07-30 14:41:20 -0400

    Beginning code for video

---
+++ src/UserInfo.js
@@ -0,0 +1,15 @@
+export const UserInfo = ({ user }) => {
+	const { name, age, hairColor, hobbies } = user || {};
+
+	return user ? (
+		<>
+		<h3>{name}</h3>
+		<p>Age: {age} years</p>
+		<p>Hair Color: {hairColor}</p>
+		<h3>Hobbies:</h3>
+		<ul>
+			{hobbies.map(hobby => <li key={hobby}>{hobby}</li>)}
+		</ul>
+		</>
+	) : <p>Loading...</p>;
+}
