ebcea8cbcc39db3599d87a3cb6e6f255a6ab77f3
Shaun Wassell <shaun@trove.ai>
2021-07-30 14:46:13 -0400

    Ending code for video

---
+++ src/App.js
@@ -0,0 +1,31 @@
+import { RecursiveComponent } from "./RecursiveComponent";
+
+const nestedObject = {
+	a: 1,
+	b: {
+		b1: 4,
+		b2: {
+			b23: 'Hello',
+		},
+		b3: {
+			b31: {
+				message: 'Hi',
+			},
+			b32: {
+				message: 'Hi',
+			}
+		}
+	},
+	c: {
+		c1: 2,
+		c2: 3,
+	}
+}
+
+function App() {
+	return (
+		<RecursiveComponent data={nestedObject} />
+	);
+}
+
+export default App;
