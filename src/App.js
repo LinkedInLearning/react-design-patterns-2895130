63678364725fc037ac77139cc4839a5f23e91d2e
Shaun Wassell <shaun@trove.ai>
2021-07-30 14:45:46 -0400

    Ending code for video

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import { printProps } from './printProps';
+import { UserInfo } from './UserInfo';
+import { withUser } from './withUser';
+
+const UserInfoWithLoader = withUser(UserInfo, '234');
+
+function App() {
+	return (
+		<UserInfoWithLoader />
+	);
+}
+
+export default App;
