798a5dc063a4bb7c6e257fbe2158df299021b638
Shaun Wassell <shaun@trove.ai>
2021-07-30 14:41:37 -0400

    Beginning code for video

---
+++ src/App.js
@@ -0,0 +1,29 @@
+const nestedObject = {
+	a: 1,
+	b: {
+		b1: 4,
+		b2: {
+			b23: 'Hello',
+		},
+		b3: {
+			b31: {
+				message: 'Hi',
+			},
+			b32: {
+				message: 'Hi',
+			}
+		}
+	},
+	c: {
+		c1: 2,
+		c2: 3,
+	}
+}
+
+function App() {
+	return (
+		<h1>Your code goes here</h1>
+	);
+}
+
+export default App;
