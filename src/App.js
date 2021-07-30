ac545f57c4176243414e676531d7c75ce3ac77f3
Shaun Wassell <shaun@trove.ai>
2021-07-30 14:41:31 -0400

    Beginning code for video

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import { UserInfo } from './UserInfo';
+
+function App() {
+	return (
+		<>
+		<UserInfo userId="123" />
+		<UserInfo userId="234" />
+		<UserInfo userId="345" />
+		</>
+	);
+}
+
+export default App;
