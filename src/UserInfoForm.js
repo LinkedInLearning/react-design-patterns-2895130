576596b830ce9485e8624517cf15309265e68e68
Shaun Wassell <shaun@trove.ai>
2021-07-30 14:45:52 -0400

    Ending code for video

---
+++ src/UserInfoForm.js
@@ -0,0 +1,24 @@
+import { withEditableUser } from "./withEditableUser";
+
+export const UserInfoForm = withEditableUser(({ user, onChangeUser, onSaveUser, onResetUser }) => {
+	const { name, age, hairColor } = user || {};
+
+	return user ? (
+		<>
+		<label>
+			Name:
+			<input value={name} onChange={e => onChangeUser({ name: e.target.value })} />
+		</label>
+		<label>
+			Age:
+			<input type="number" value={age} onChange={e => onChangeUser({ age: Number(e.target.value) })} />
+		</label>
+		<label>
+			Hair Color:
+			<input value={hairColor} onChange={e => onChangeUser({ hairColor: e.target.value })} />
+		</label>
+		<button onClick={onResetUser}>Reset</button>
+		<button onClick={onSaveUser}>Save Changes</button>
+		</>
+	) : <p>Loading...</p>;
+}, '123');
